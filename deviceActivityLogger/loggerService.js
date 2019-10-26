5dfd0d02f95704c2876a93a4ab7fc16087bb5159
safa19 <safagunay96@gmail.com>
2019-10-26 20:43:36 +0300

    include 'from' option while creating contract

--- deviceActivityLogger/loggerService.js
+++ deviceActivityLogger/loggerService.js
@@ -2,7 +2,9 @@ const Web3 = require("web3");
 const abi = require("./abi.json");
 const address = Web3.utils.toChecksumAddress(process.env.CONTRACT_ADDRESS);
 const web3 = new Web3(process.env.REMOTE_NODE);
-const contract = new web3.eth.Contract(abi, address);
+const contract = new web3.eth.Contract(abi, address, {
+    from: process.env.CONTRACT_OWNER_ACCOUNT
+});
 const NonceModel = require("./nonceModel");
 
 const addLogToDevice = async (devCode, log) => {
